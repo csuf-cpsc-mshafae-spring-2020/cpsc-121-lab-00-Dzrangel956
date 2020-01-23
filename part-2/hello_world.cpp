0aec676ef5cc4d2d835edb371cd160b7b592291c
Dzrangel956 <dzrangel956@cs104u-3c.acad.fullerton.edu>
2020-01-23 15:46:35 -0800

    completed lab 1

--- part-2/hello_world.cpp
+++ part-2/hello_world.cpp
@@ -1,11 +1,22 @@
-// Oscar Mayer
-// CPSC 121-99
-// oscar.mayer@csu.fullerton.edu
+// David Rangel
+// CPSC 121-03
+// Dzrangel956@csu.fullerton.edu
 
 #include <iostream>
 using namespace std;
 
 int main(int argc, char *argv[]){
-  cout << "Hello world!I really like programming" << endl;
+  if (argc<3)
+  {
+    cout << "Too few arguments, please enter <Word> followed by number of times to print.\n";
+    return 1;
+  }
+
+  string word =argv[1];
+  int num = stoi (argv[2]);
+  for (int i=0; i < num; i++)
+  {
+  cout << "Hello "<<word << endl;
+}
   return 0;
 }
